8f14ce30be64a670ef0dd4267dee399896d1375a
Campbell Jones <git@serebit.com>
2020-09-05 18:40:14 -0400

    Move C source to oldsrc

---
+++ src/qol-assist/main.go
@@ -0,0 +1,18 @@
+/*
+ * This file is part of qol-assist.
+ *
+ * Copyright © 2017-2020 Solus Project
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
